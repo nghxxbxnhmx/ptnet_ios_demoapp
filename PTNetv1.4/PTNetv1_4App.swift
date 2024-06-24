957bd26df3a6c569ef2425594383e95a3731539b
nghiaphan1411 <tthanhnghia1411@gmail.com>
2024-06-24 15:20:27 +0700

    Initial Commit

---
+++ PTNetv1.4/PTNetv1_4App.swift
@@ -0,0 +1,17 @@
+//
+//  PTNetv1_4App.swift
+//  PTNetv1.4
+//
+//  Created by Nguyen Huu Tuong on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct PTNetv1_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
